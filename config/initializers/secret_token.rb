df0507fa99e98117cf6218474b18c0e2233395e4
Christine Lam <christine_pirate93@live.com>
2013-11-19 11:36:53 -0700

    adding foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IdeaHub::Application.config.secret_key_base = '91744180d757899e17a98629f25f39490c75bfeada71e71c2a069cd6173b88eca3d7c132dd8f46c8d5898dd7e10b03ece8f543e3824d4021bc5974b6b77d53f6'
