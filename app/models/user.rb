30a778ed1d50d1e5b2f2abaf3259857537476d5c
Alexa Boccieri <aeboccie@asu.edu>
2013-11-24 20:47:42 -0700

    Added Devise Gem, User Model and Bootstrap Stylesheets

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+         before_save :default_values
+         def default_values
+           self.reputation ||= '0'
+         end
+end
