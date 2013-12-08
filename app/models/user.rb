5d8f527091be2b14c502411ea4081cb08551bb45
Sunnygolden8 <Sunnygolden8@users.noreply.github.com>
2013-12-08 11:50:21 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,19 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
-=======
+
   has_merit
 
->>>>>>> 33d82b381f4ba8f0bb6d65c84c2c6661e3bd943f
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   has_many :ratings
   has_many :comments
-<<<<<<< HEAD
+
   has_many :projects
-=======
-<<<<<<< HEAD
-  has_many :projects
-=======
->>>>>>> 33d82b381f4ba8f0bb6d65c84c2c6661e3bd943f
->>>>>>> clam4-master
+
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
-<<<<<<< HEAD
-=======
+
   def total_points 
       if user_signed_in?
           self.rewardPoints += 2
@@ -36,7 +27,5 @@ class User < ActiveRecord::Base
     self.rewardPoints = cur_rewardPoints - amt
     self.save
   end
-
->>>>>>> 33d82b381f4ba8f0bb6d65c84c2c6661e3bd943f
   
 end
