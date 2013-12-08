985e2a1562c0152b7d107f35940f86d745ab1acf
Sunnygolden8 <Sunnygolden8@users.noreply.github.com>
2013-12-08 11:59:43 -0700

    Update project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,6 @@
 class Project < ActiveRecord::Base
 belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-<<<<<<< HEAD
+
 has_many :comments
-=======
-<<<<<<< HEAD
-has_many :comments
-=======
->>>>>>> 33d82b381f4ba8f0bb6d65c84c2c6661e3bd943f
->>>>>>> clam4-master
+
 end
