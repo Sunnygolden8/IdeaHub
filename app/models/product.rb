33113e621156766ceb953fd6143964e20b813db0
Sunnygolden8 <Sunnygolden8@users.noreply.github.com>
2013-12-08 11:57:21 -0700

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,11 +2,9 @@ class Product < ActiveRecord::Base
   default_scope :order => 'title'
   
   has_many :line_items
-<<<<<<< HEAD
-=======
+
   has_many :orders, :through => :line_items
   
->>>>>>> 33d82b381f4ba8f0bb6d65c84c2c6661e3bd943f
   before_destroy :ensure_not_referenced_by_any_line_item
   # ensure that there are no line items referencing this product
   def ensure_not_referenced_by_any_line_item
